54bb88d3d823a81b5aaff196fc9ebf0161d57131
Luna25662 <homero.hernandez5640@gmail.com>
2026-03-07 23:21:55 -0600

    Final touches
    Just some light touches and documentation

--- Main.java
+++ Main.java
@@ -18,7 +18,9 @@ String Fifth;
   //List of possible words that might be used
   String[] Words = {"World","Steve","Great","Plant", "Daily", "Broke", "Cheap", "Brown", "Chest", "Dated", "Cycle", "Check", "Bring"};
  String word = Words[random.nextInt(0,13)];
+System.out.println("This is a 5 letter word");
 
+//Checks the 5 choices
 First = scanner.nextLine();
   wordCheck(word, First);
   System.out.println("**************************************");
@@ -38,11 +40,13 @@ First = scanner.nextLine();
   Fifth = scanner.nextLine();
   wordCheck(word,Fifth);
     System.out.println("**************************************");
+    System.out.println("The word was: "+ word);
   scanner.close();
   
     }    
 static void wordCheck(String word, String response){
 int j = 0;
+//Checks each letter to see if it corresponds or not
 for(int i = 0; i<5; i++){
   j = j+1;
  String Wword = word.substring(i,i+1);
